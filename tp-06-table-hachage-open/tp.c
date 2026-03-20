b8902b9a8952399fce5fa940cbf78994fb16bf03
MatisseAD <matisse.martayan@gmail.com>
2026-03-20 15:36:38 +0100

    Fin du TP

--- tp-06-table-hachage-open/tp.c
+++ tp-06-table-hachage-open/tp.c
@@ -6,6 +6,7 @@
 
 const uint8_t empty = 0;
 const uint8_t occupied = 1;
+const uint8_t tombstone = 2;
 typedef uint32_t T;
 
 struct bucket {
@@ -86,20 +87,6 @@ donnera de plus un argument justifiant la terminaison de cette fonction
 
 */
 
-bool set_is_member(set *s, T x) {
-    uint64_t i = hash(x, s->p);
-    int ans = false;
-
-    while (s->a[i].statuts != empty && !(ans)) {
-        if (s->a[i].element == x) {
-            ans = true;
-        }
-        i = (i + 1) % s->p;
-    }
-
-    return ans;
-}
-
 T set_get(set *s, uint64_t i) {
 
     return s->a[i].element;
@@ -109,11 +96,11 @@ uint64_t set_begin(set *s) {
     int i = 0;
     int ans = 0;
 
-    if (s->a[0].statuts = occupied) {
+    if (s->a[0].statuts == occupied) {
         return ans;
     }
 
-    while (ans == 0 && i < s->p) {
+    while (ans == 0 && i < power_of_2(s->p)) {
         if (s->a[i].statuts == occupied) {
             ans = i;
         }
@@ -146,6 +133,166 @@ uint64_t set_next(set *s, uint64_t i) {
 
     return ans;
 }
+
+uint64_t set_search(set *s, T x, bool *found) {
+    uint64_t i = hash(x, s->p);
+    bool ans = false;
+    int size = power_of_2(s->p);
+
+    while (s->a[i].statuts != empty && !(ans)) {
+        if (s->a[i].element == x) {
+            ans = true;
+        }
+        i = (i + 1) % (size);
+    }
+
+    (*found) = ans;
+
+    return i - 1;
+}
+
+bool set_is_member(set *s, T x) {
+    bool is_here;
+    uint64_t i = set_search(s, x, &is_here);
+
+    return is_here;
+}
+
+void set_resize(set *s, int p) {
+    set *new_s = malloc(sizeof(set));
+    uint64_t new_size = power_of_2(p);
+    new_s->p = p;
+    new_s->nb_empty = new_size;
+    new_s->a = malloc(new_size * sizeof(bucket));
+}
+
+void set_resize(set *s, int p) {
+    set *new_set = malloc(sizeof(set));
+
+    uint64_t new_size = power_of_2(p);
+    new_set->nb_empty = new_size;
+    new_set->p = p;
+    new_set->a = malloc(new_size * sizeof(bucket));
+
+    for (int i; i < new_size; i++) {
+        s->a[i].statuts = empty;
+    }
+
+    int i = set_begin(s);
+
+    while (i < set_end(s)) {
+        T value = s->a[i].element;
+        uint64_t indice_case = hash(value, p);
+
+        while (s->a[i].statuts != empty) {
+            indice_case++;
+        }
+
+        new_set->a[indice_case].element = value;
+        new_set->a[indice_case].statuts = occupied;
+        new_set->nb_empty--;
+
+        i = set_next(s, i);
+    }
+
+    free(s->a);
+    *s = *new_set;
+    free(new_set);
+}
+
+/**
+ * 
+ * 3. Écrire la fonction void set_add(set *s, T x) ajoutant l’élément x à la table s. Afin de toujours conserver
+une case « libre » dans notre tableau et de ne pas trop le charger, on décidera de doubler la taille du tableau
+dès que le nombre de cases « libres » est inférieur au tiers de la taille du tableau
+ * 
+ */
+
+void set_add(set *s, T x) {
+    bool no_useful = false;
+
+    if (s->nb_empty <= s->nb_empty * (1 / 3)) {
+        set_resize(s, s->p * 2);
+    }
+
+    if (!set_is_member(s, x)) {
+        uint64_t i = set_search(s, x, &no_useful);
+        s->a[i].element = x;
+        s->a[i].statuts = occupied;
+        s->nb_empty--;
+    }
+}
+
+// 4. Suppression d'éléments
+
+/*
+
+1. Expliquer pourquoi il n’est pas possible de supprimer un élément de la table en changeant simplement le statut
+de sa case en case « libre ».
+
+Si on met une telle case vide, alors la recherche d'un élément peut être fausse.
+
+*/
+
+/*
+
+2. Proposer des nouvelles implémentations des fonctions set_search, set_begin, set_next et set_add fonction-
+nant avec les pierres tombales.
+
+*/
+
+uint64_t set_search(set *s, T x, bool *found) {
+    uint64_t i = hash(x, s->p);
+    bool ans = false;
+    int size = power_of_2(s->p);
+    int ftb = NULL;
+
+    while (s->a[i].statuts != empty && !(ans)) {
+        if (s->a[i].statuts == tombstone && ftb == NULL) {
+            ftb = i;
+        }
+
+        if (s->a[i].element == x && s->a[i].statuts != tombstone) {
+            ans = true;
+        }
+
+        i = (i + 1) % size;
+    }
+
+    (*found) = ans;
+
+    if (!ans) {
+        if (ftb != NULL) {
+            return ftb;
+        }
+    } else {
+    }
+
+
+    return i - 1;
+}
+
+uint64_t set_begin(set *s) {
+    int i = 0;
+    int ans = 0;
+
+    if (s->a[0].statuts = occupied) {
+        return ans;
+    }
+
+    while (ans == 0 && i < s->p) {
+        if (s->a[i].statuts == occupied) {
+            ans = i;
+        }
+        i++;
+    }
+
+    if (ans == 0) {
+        return power_of_2(s->p);
+    }
+    return ans;
+}
+
 
 int main(void) {
 
